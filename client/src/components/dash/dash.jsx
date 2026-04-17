f62a2815722966ba799b75d2d6742230b6100df9
burity2 <arthur.rosa66@gamil.com>
2026-04-17 11:17:24 -0300

    basic userpage whipped up

--- client/src/components/dash/dash.jsx
+++ client/src/components/dash/dash.jsx
@@ -8,8 +8,29 @@ export default function Dashboard() {
 
   return (
     <>
-    <h1>candidate page</h1>
-    <p>Candidate id: { id }</p>
+    <div id='dash-container'>
+      <div id="dash-side-menu">
+        <div id="user">
+          <div id="user-img" className='user-icons'>😄</div>
+          <p id="username">{id}</p>
+        </div>
+        <div id="config" className='user-icons'>⚙️</div>
+      </div>
+      <div id="dash-main">
+        <div id="dash-header">
+          <div id="notifications">🔔</div>
+          <div id="warning">⚠️</div>
+        </div>
+        <div id="dash-steps">
+          <div className="step">1</div>
+          <div className="step">2</div>
+          <div className="step">3</div>
+          <div className="step">4</div>
+          <div className="step">5</div>
+          <div className="step">6</div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
