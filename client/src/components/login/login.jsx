a293e48be896b5027280c4220ade319420d3a656
burity2 <arthur.rosa66@gamil.com>
2026-04-23 20:54:27 -0300

    design update on progress

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,5 +1,6 @@
 import './login.css'
-import logo from '../../assets/logo-color.png'
+import logo from '../../assets/logo-color-w.png'
+import loginImg from '../../assets/bg-imgs/login.png'
 
 export default function Login({handleSubmit}) {
 
@@ -8,32 +9,51 @@ export default function Login({handleSubmit}) {
     <div id="login-container">
       <div id="art-div">
         <div id='img-container'>
-          <img src="" alt="" />
+          <img src={loginImg} alt="" />
         </div>
-        <div id="eb-logo"></div>
-        <div id="art-txt">
-          <h1 id='welcome-title'></h1>
-          <p id='welcome-txt'></p>
+        <div id='info'>
+          <div id="eb-logo-container"><img id='eb-img' src={logo} alt="" /></div>
+            <div id="art-txt">
+              <h1 id='welcome-title'>Welcome to Ensina Brasil's <h1 id='welcome-title-highlight'>Admission Process</h1></h1>
+              <p id='welcome-txt'>Log in to access the admission process and track your Trainee journey</p>
+            </div>
+          </div>
         </div>
-      </div>
       <div id="login-form">
+        <div id="dark-mode-toggler">
+          <button>sunenmoon</button>
+        </div>
         <div id="form-title">
-          <img id='logo-img' src={logo} alt="" />
-          <p className="data-form-title">Access your account</p>
+          <h1 className="data-form-title">Access your account</h1>
+          <p>Use you email and password to continue</p>
         </div>
         <div id="form-body">
           <form action="" onSubmit={handleSubmit}>
             <div className="form-group">
-              <label htmlFor="email">EMAIL</label>
+              <label htmlFor="email">E-Mail</label>
+              <i className="input-icon"></i>
               <input type="email" name="email" id="email" placeholder='john.smith@mail.com' required/>
             </div>
             <div className="form-group">
-              <label htmlFor="password">PASSWORD</label>
+              <label htmlFor="password">Password</label>
+              <i className="input-icon"></i>
               <input type="password" name="password" id="password" minLength="4" required/>
+              <button className="input-btn"></button>
             </div>
-              <input type="submit" name="sumbmit" id="submit" value={'Submit'}/>
+              <p>I forgot my password</p>
+              <input type="submit" name="sumbmit" id="submit" value={'Log in'}/>
           </form>
+          <div id='divider'>
+            <span className='line'></span>
+            <p id='divider-txt'>or</p>
+            <span className='line'></span>
+          </div>
+          <button id='google-enter-btn'>
+            <img src="" alt="" />
+            <p>Login with Google</p>
+            </button>
         </div>
+        <p>If you haven't created an account yet, <a href="">clik here to create it</a></p>
       </div>
     </div>
     </>
