b98ee8d9b6bbc5eb0a398c12fe7f16b6f0aef0a5
Adam Coe <adam@grantgenie.uk>
2026-04-27 13:06:58 +0200

    Fix login layout

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -5,57 +5,57 @@ import loginImg from '../../assets/bg-imgs/login.png'
 export default function Login({handleSubmit}) {
 
   return (
-    <>
     <div id="login-container">
-      <div id="art-div">
-        <div id='img-container'>
-          <img src={loginImg} alt="" />
-        </div>
-        <div id='info'>
-          <div id="eb-logo-container"><img id='eb-img' src={logo} alt="" /></div>
-            <div id="art-txt">
-              <h1 id='welcome-title'>Welcome to Ensina Brasil's <h1 id='welcome-title-highlight'>Admission Process</h1></h1>
-              <p id='welcome-txt'>Log in to access the admission process and track your Trainee journey</p>
-            </div>
+      <div id="img-container">
+        <img className="bg-decoration" src={loginImg} alt="" aria-hidden="true" />
+  
+        <div id="art-div">
+          <div id="eb-logo-container">
+            <img id="eb-img" src={logo} alt="Ensina Brasil" />
+          </div>
+          <div id="art-txt">
+            <h1 id="welcome-title">
+              Welcome to Ensina Brasil's{" "}
+              <span id="welcome-title-highlight">Admission Process</span>
+            </h1>
+            <p id="welcome-txt">
+              Log in to access the admission process and track your Trainee journey
+            </p>
           </div>
         </div>
-      <div id="login-form">
-        <div id="dark-mode-toggler">
-          <button>sunenmoon</button>
-        </div>
-        <div id="form-title">
-          <h1 className="data-form-title">Access your account</h1>
-          <p>Use you email and password to continue</p>
-        </div>
-        <div id="form-body">
-          <form action="" onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="email">E-Mail</label>
-              <i className="input-icon"></i>
-              <input type="email" name="email" id="email" placeholder='john.smith@mail.com' required/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <i className="input-icon"></i>
-              <input type="password" name="password" id="password" minLength="4" required/>
-              <button className="input-btn"></button>
-            </div>
-              <p>I forgot my password</p>
-              <input type="submit" name="sumbmit" id="submit" value={'Log in'}/>
-          </form>
-          <div id='divider'>
-            <span className='line'></span>
-            <p id='divider-txt'>or</p>
-            <span className='line'></span>
+  
+        <div id="login-form">
+          <div id="form-title">
+            <h1 className="data-form-title">Access your account</h1>
+            <p>Use your email and password to continue</p>
           </div>
-          <button id='google-enter-btn'>
-            <img src="" alt="" />
-            <p>Login with Google</p>
+          <div id="form-body">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="email">E-Mail</label>
+                <input type="email" name="email" id="email" placeholder="john.smith@mail.com" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" minLength={4} required />
+              </div>
+              <p className="forgot">I forgot my password</p>
+              <input type="submit" name="submit" id="submit" value="Log in" />
+            </form>
+            <div id="divider">
+              <span className="line"></span>
+              <p id="divider-txt">or</p>
+              <span className="line"></span>
+            </div>
+            <button type="button" id="google-enter-btn">
+              <p>Login with Google</p>
             </button>
+          </div>
+          <p className="signup-prompt">
+            If you haven't created an account yet, <a href="">click here to create it</a>
+          </p>
         </div>
-        <p>If you haven't created an account yet, <a href="">clik here to create it</a></p>
       </div>
     </div>
-    </>
-  )
+  );
 }
